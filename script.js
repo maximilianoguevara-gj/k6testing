c917c3b506ab3ecd8934475eb07d46b744f9ba29
maximilianoguevara-gf <112887426+maximilianoguevara-gj@users.noreply.github.com>
2023-09-22 10:51:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+import { browser } from 'k6/experimental/browser';
+import { check } from 'k6';
+import http from 'k6/http';
+
+export const options = {
+  scenarios: {
+    browser: {
+      executor: 'constant-vus',
+      exec: 'browserTest',
+      vus: 20,
+      duration: '5m',
+      options: {
+        browser: {
+          type: 'chromium',
+        }
+      }
+    },
+    news: {
+      executor: 'constant-vus',
+      exec: 'news',
+      vus: 20,
+      duration: '5m',
+    }
+  }
+};
+
+export async function browserTest() {
+  const page = browser.newPage();
+
+  try {
+    await page.goto('https://live.gojiraf.com/store/64ea016f2d4eb90012f53913?utm_source=slug_gojiraf&utm_medium=social_sharing_Meli&utm_campaign=MELI');
+
+    const button = page.locator('#like-button');
+    await button.click()
+
+    
+  } finally {
+    page.close();
+  }
+}
+
+export function news() {
+  const res = http.get('https://live.gojiraf.com/store/64ea016f2d4eb90012f53913?utm_source=slug_gojiraf&utm_medium=social_sharing_Meli&utm_campaign=MELI');
+  const button = page.locator('#like-button');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    
+  });
+}
